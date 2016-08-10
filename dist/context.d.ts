4f5aa7d1c08f917b096289efd22f33549490ccf6
Qiaosen Huang <joesonw@gmail.com>
2016-08-10 15:06:55 +0800

    allow add extra field to Context.

--- dist/context.d.ts
+++ dist/context.d.ts
@@ -2,6 +2,7 @@
 import * as Koa from 'koa';
 import Cookie from './cookie';
 interface Context extends Koa.Context {
+    [key: string]: any;
     cookie?: Cookie;
     params?: Object;
     requestBody?: any;
--- src/context.ts
+++ src/context.ts
@@ -3,6 +3,7 @@ import * as Koa from 'koa';
 import Cookie from './cookie';
 
 interface Context extends Koa.Context{
+    [key: string]: any;
     cookie?: Cookie;
     params?: Object;
     requestBody?: any;
